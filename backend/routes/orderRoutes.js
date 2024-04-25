9f30424bc39fa0628ce1270dc1b8a1ae3d18d930
Napoleon Torres <159686382+NapsTorres@users.noreply.github.com>
2024-04-26 07:43:47 +0800

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -7,14 +7,15 @@ import {
   updateOrderToDelivered,
   getOrders,
 } from '../controllers/orderController.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
+// import { protect, admin } from '../middleware/authMiddleware.js'; // Remove protect middleware
 
 const router = express.Router();
 
-router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
-router.route('/mine').get(protect, getMyOrders);
-router.route('/:id').get(protect, getOrderById);
-router.route('/:id/pay').put(protect, updateOrderToPaid);
-router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
+// Remove protect middleware from routes
+router.route('/').post(addOrderItems).get(admin, getOrders);
+router.route('/mine').get(getMyOrders);
+router.route('/:id').get(getOrderById);
+router.route('/:id/pay').put(updateOrderToPaid);
+router.route('/:id/deliver').put(admin, updateOrderToDelivered);
 
 export default router;
